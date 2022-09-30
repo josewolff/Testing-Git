2506e4f996e946201e04a89f0cbfa3c7f63c9ca5
Jose Wolff <josewolff@Joses-MacBook-Pro.local>
2022-09-29 20:34:06 -0600

    integration testing

---
+++ src/test/java/apiTesting/APITest.java
@@ -0,0 +1,46 @@
+package apiTesting;
+
+import apiTestingAuxClasses.RequestMaker;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import utils.GlobalVariables;
+
+public class APITest {
+
+    @Test(groups = {"all","get"})
+    public void getTest(){
+        String getAllUsers = GlobalVariables.apiHost  + "/users/all";
+        Response response = RequestMaker.makeGetRequest(getAllUsers);
+        String responseString = response.asString();
+        System.out.println(responseString);
+        JSONArray jsonArray =  new JSONArray(responseString);
+        System.out.println(jsonArray.toString(10));
+        if(jsonArray.length() == 0){
+            Assert.fail("Fail test - Get Users is empty.");
+        }
+    }
+
+    @Test(groups = {"all","post"})
+    public void postMethod(){
+        String getAllUsers = GlobalVariables.apiHost  + "/users/add";
+        for (int i = 0; i < 300; i++){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("firstname", "Wolff" + i)
+                    .put("lastname", "Jose" + i)
+                    .put("username","josewolff"+ i)
+                    .put("email","wolff@laserants.com"+ i);
+            Response response = RequestMaker.makePostRequest(getAllUsers, jsonObject.toString());
+            System.out.println(response.asString());
+        }
+
+    }
+
+    @AfterClass
+    public void after(){
+        System.out.println("After");
+    }
+}
